ee505fac053e6b2dbe354b25279d7bacd236811b
Tom C <tom.carlier@epitech.eu>
2024-03-10 19:17:37 +0100

    [feat] -->  Add the makefile include/ and src/ with return 84

---
+++ include/my.h
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2023
+** MY_H
+** File description:
+** Placeholder
+*/
+
+#ifndef MY_H
+    #define MY_H
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <stdint.h>
+    #include <time.h>
+    #include <limits.h>
+    #include <stdarg.h>
+    #include <errno.h>
+    #include <math.h>
+    #include <stdlib.h>
+    #include <signal.h>
+    #include <stdbool.h>
+    #include <string.h>
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <sys/wait.h>
+    #include <fcntl.h>
+
+#endif
