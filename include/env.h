24e39a133e2d1920fcdc5ab570e06ea01d93b3d9
Tom C <tom.carlier@epitech.eu>
2024-03-19 00:20:43 +0100

    [feat] -->  add the env.h

---
+++ include/env.h
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2023
+** MY_H
+** File description:
+** Placeholder
+*/
+
+#ifndef ENV_H
+    #define ENV_H
+
+    #include <stdbool.h>
+
+char *env_val_concat(char const *, char const *);
+bool env_val_is_good(char const *);
+char **env_add(char **, char const *);
+bool env_contains(char **, char const *);
+char **env_remove(char **, char const *);
+char **env_cp(char **);
+int env_destroy(char **);
+
+#endif
