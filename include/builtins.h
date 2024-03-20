a144be26775de94820e7f14d4b206b0b86e9b5c6
Tom C <tom.carlier@epitech.eu>
2024-03-20 10:10:44 +0100

    [feat] -->  add the setenv builtin

---
+++ include/builtins.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** MY_H
+** File description:
+** Placeholder
+*/
+
+#ifndef BUILTINS_H
+    #define BUILTINS_H
+
+    #include "shell.h"
+
+int builtin_setenv(shell_t *, int, char **);
+
+#endif
