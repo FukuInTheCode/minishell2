55aabbdde7b84dc7c1021900e5c59615db0ad225
Tom C <tom.carlier@epitech.eu>
2024-03-20 19:42:10 +0100

    [feat] -->  add the path concat function

---
+++ include/path.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** MY_H
+** File description:
+** Placeholder
+*/
+
+#ifndef PATH_H
+    #define PATH_H
+
+char *path_concat(char const *, char const *);
+
+#endif
---
+++ src/path/path_concat.c
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2024
+** path_concat.c
+** File description:
+** path_concat.c
+*/
+
+#include "my.h"
+
+static char *create_path(char const *former, char const *latter,
+    bool has_slash)
+{
+    char *path = malloc(my_strlen(former) + my_strlen(latter) + 1 + has_slash);
+
+    if (!path)
+        return NULL;
+    my_memset(path, 0, my_strlen(former) + my_strlen(latter) + 1 + has_slash);
+    my_strcat(path, former);
+    if (!has_slash)
+        my_strcat(path, "/");
+    my_strcat(path, latter);
+    return path;
+}
+
+char *path_concat(char const *former, char const *latter)
+{
+    bool has_slash = false;
+
+    if (!former && !latter)
+        return NULL;
+    if (!former)
+        return my_strdup(latter);
+    if (!latter)
+        return my_strdup(former);
+    if (former[my_strlen(former) - 1] == '/' || *latter == '/')
+        has_slash = true;
+    return create_path(former, latter, has_slash);
+}
