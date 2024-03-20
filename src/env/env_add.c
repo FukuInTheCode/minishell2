f1e95758d9786895e6ab591255750cf92cf4b4c9
Tom C <tom.carlier@epitech.eu>
2024-03-20 10:12:27 +0100

    [refactor] -->  change the env_add function to take 3 args instead of 2

--- src/env/env_add.c
+++ src/env/env_add.c
@@ -7,12 +7,12 @@
 
 #include "my.h"
 
-char **env_add(char **env, char const *new)
+char **env_add(char **env, char const *var, char const *val)
 {
     char **new_env = NULL;
     size_t len = 0;
 
-    if (!env || !new)
+    if (!env || !var)
         return env;
     len = env_len(env);
     new_env = malloc(sizeof(char *) * (len + 2));
@@ -21,6 +21,6 @@ char **env_add(char **env, char const *new)
     my_memset((void *)new_env, 0, sizeof(char *) * (len + 2));
     for (size_t i = 0; i < len; i++)
         new_env[i] = my_strdup(env[i]);
-    new_env[len] = my_strdup(new);
+    new_env[len] = env_val_concat(var, val);
     return new_env;
 }
