55dc899e7dd0967d45890d130cfcb66092411146
Tom C <tom.carlier@epitech.eu>
2024-03-23 01:50:56 +0100

    [refactor] -->  change the parser_command()

--- src/parser/parser_command.c
+++ src/parser/parser_command.c
@@ -7,15 +7,13 @@
 
 #include "my.h"
 
-static int add_to_arr(command_t ***arr, type_t t, bool *is_basic,
-    char const *input)
+static bool add_to_arr(command_t ***arr, type_t t, char **input, size_t *i)
 {
     command_t *command = command_create();
 
     command_set_type(command, t);
     *arr = command_array_add(*arr, command);
-    *is_basic = false;
-    return 0;
+    return false;
 }
 
 static int setup_basic(bool *is_basic, command_t ***arr, char *input)
@@ -28,7 +26,7 @@ static int setup_basic(bool *is_basic, command_t ***arr, char *input)
     }
     command = command_create();
     command_set_type(command, COMMAND);
-    command_set_argv(command, my_str_to_word_array(input, " \t"));
+    command_set_argv(command, my_str_to_word_array(input, " \t\n"));
     command_set_argc(command, my_len_word_array(command->argv));
     *arr = command_array_add(*arr, command);
     return 0;
@@ -37,22 +35,22 @@ static int setup_basic(bool *is_basic, command_t ***arr, char *input)
 command_t **parser_command(parser_t *parser)
 {
     command_t **arr = command_array_create();
-    bool is_basic = true;
+    bool is_base = true;
 
     for (size_t i = 0; parser->parsed_input[i]; i++) {
         if (!my_strcmp(parser->parsed_input[i], SEMICOLON))
-            add_to_arr(&arr, END, &is_basic, parser->parsed_input[i]);
+            is_base = add_to_arr(&arr, END, parser->parsed_input, &i);
         if (!my_strcmp(parser->parsed_input[i], PIPE))
-            add_to_arr(&arr, PIPE_T, &is_basic, parser->parsed_input[i]);
+            is_base = add_to_arr(&arr, PIPE_T, parser->parsed_input, &i);
         if (!my_strcmp(parser->parsed_input[i], DOUBLE_RIGHT))
-            add_to_arr(&arr, RIGHT_DBLRED, &is_basic, parser->parsed_input[i]);
+            is_base = add_to_arr(&arr, RIGHT_DBLRED, parser->parsed_input, &i);
         if (!my_strcmp(parser->parsed_input[i], DOUBLE_LEFT))
-            add_to_arr(&arr, LEFT_DBLRED, &is_basic, parser->parsed_input[i]);
+            is_base = add_to_arr(&arr, LEFT_DBLRED, parser->parsed_input, &i);
         if (!my_strcmp(parser->parsed_input[i], SINGLE_RIGHT))
-            add_to_arr(&arr, RIGHT_RED, &is_basic, parser->parsed_input[i]);
+            is_base = add_to_arr(&arr, RIGHT_RED, parser->parsed_input, &i);
         if (!my_strcmp(parser->parsed_input[i], SINGLE_RIGHT))
-            add_to_arr(&arr, LEFT_RED, &is_basic, parser->parsed_input[i]);
-        setup_basic(&is_basic, &arr, parser->parsed_input[i]);
+            is_base = add_to_arr(&arr, LEFT_RED, parser->parsed_input, &i);
+        setup_basic(&is_base, &arr, parser->parsed_input[i]);
     }
     return arr;
 }
