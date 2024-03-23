3db5bca81b2ac8cb9e496965608ab09dba30800e
Tom C <tom.carlier@epitech.eu>
2024-03-23 14:20:53 +0100

    [fix] --> errro for parser_parse

--- src/parser/parser_parse.c
+++ src/parser/parser_parse.c
@@ -9,8 +9,10 @@
 
 int parser_parse(parser_t *parser, char const *input)
 {
-    parser_add(parser, my_strdup(input));
+    if (parser_add(parser, my_strdup(input)) == 84)
+        return 84;
     for (size_t i = 0; parsings_functions[i]; i++)
-        parsings_functions[i](parser);
+        if (parsings_functions[i](parser) == 84)
+            return 84;
     return 0;
 }
