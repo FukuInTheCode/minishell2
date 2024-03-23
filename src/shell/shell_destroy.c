62b08b831cb6ae3f8a42122f2561c0784e83fa12
Tom C <tom.carlier@epitech.eu>
2024-03-23 14:11:45 +0100

    [fix] --> add the error handling for the shell_destroy

--- src/shell/shell_destroy.c
+++ src/shell/shell_destroy.c
@@ -9,13 +9,15 @@
 
 int shell_destroy(shell_t *shell)
 {
-    int return_val = shell->return_code;
+    int return_val = 0;
 
+    if (!shell)
+        return 0;
+    return_val = shell->return_code;
     free(shell->user_input);
     free(shell->oldpwd);
     free(shell->prompt);
     command_array_destroy(shell->cmds);
     free(shell);
-    exit(return_val);
-    return 0;
+    return return_val;
 }
