01515fd1f3523e233c5c6bc80ea2eb22b478b4c3
Tom C <tom.carlier@epitech.eu>
2024-03-25 15:46:12 +0100

    [feat] -->  add the erro handling for redictions

--- src/command/array/command_array_redirection.c
+++ src/command/array/command_array_redirection.c
@@ -8,11 +8,37 @@
 #include "command.h"
 #include "my.h"
 
+static int handle_file_error(char const *path, bool check_r, bool check_w)
+{
+    if (!path_exist(path)) {
+        my_dputs(2, path);
+        my_dputs(2, ": No such file or directory.\n");
+        return 0;
+    }
+    if (check_r && !path_readable(path)) {
+        my_dputs(2, path);
+        my_dputs(2, ": Permission denied.\n");
+        return 0;
+    }
+    if (check_w && !path_writable(path)) {
+        my_dputs(2, path);
+        my_dputs(2, ": Permission denied.\n");
+        return 0;
+    }
+    return 0;
+}
+
 static int create_left_redirect(command_t *before, command_t *curr)
 {
     if (!curr->argv || !*curr->argv || !path_readable(*curr->argv) ||
-        curr->type != FILE_T)
+        curr->type != FILE_T) {
+        handle_file_error(*curr->argv, 1, 0);
         return 1;
+    }
+    if (before->in != SYS_IN) {
+        my_dputs(2, "Ambiguous input redirect.\n");
+        return 1;
+    }
     before->in = open(*curr->argv, O_RDONLY);
     if (before->in < 0)
         perror("open");
@@ -22,8 +48,14 @@ static int create_left_redirect(command_t *before, command_t *curr)
 static int create_right_redirect(command_t *before, command_t *curr)
 {
     if (!curr->argv || !*curr->argv || !path_writable(*curr->argv) ||
-        curr->type != FILE_T)
+        curr->type != FILE_T) {
+        handle_file_error(*curr->argv, 0, 1);
         return 1;
+    }
+    if (before->out != SYS_OUT) {
+        my_dputs(2, "Ambuguous output redirect.\n");
+        return 1;
+    }
     before->out = open(*curr->argv, O_CREAT | O_WRONLY);
     if (before->out < 0)
         perror("open");
@@ -33,8 +65,14 @@ static int create_right_redirect(command_t *before, command_t *curr)
 static int create_dblright_redirect(command_t *before, command_t *curr)
 {
     if (!curr->argv || !*curr->argv || !path_writable(*curr->argv) ||
-        curr->type != FILE_T)
+        curr->type != FILE_T) {
+        handle_file_error(*curr->argv, 0, 1);
         return 1;
+    }
+    if (before->out != SYS_OUT) {
+        my_dputs(2, "Ambuguous output redirect.\n");
+        return 1;
+    }
     before->out = open(*curr->argv, O_CREAT | O_WRONLY | O_APPEND);
     if (before->out < 0)
         perror("open");
@@ -49,13 +87,17 @@ static size_t find_before(command_t **arr, size_t i)
 
 int command_array_redirection(command_t **arr)
 {
+    int error = 0;
+
     for (size_t i = 0; arr[i]; i++) {
         if (arr[i]->type == LEFT_RED)
-            create_left_redirect(arr[i - 1], arr[i + 1]);
+            error = create_left_redirect(arr[i - 1], arr[i + 1]);
         if (arr[i]->type == RIGHT_RED)
-            create_right_redirect(arr[find_before(arr, i)], arr[i + 1]);
+            error = create_right_redirect(arr[find_before(arr, i)], arr[i + 1]);
         if (arr[i]->type == RIGHT_DBLRED)
-            create_dblright_redirect(arr[find_before(arr, i)], arr[i + 1]);
+            error = create_dblright_redirect(arr[find_before(arr, i)], arr[i + 1]);
+        if (error)
+            return error;
     }
     return 0;
 }
