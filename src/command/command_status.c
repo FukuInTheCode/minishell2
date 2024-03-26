ed4c3038248fe76e12ed103ef32681107e39fe62
Tom C <tom.carlier@epitech.eu>
2024-03-26 02:21:13 +0100

    [feat] -->  add the status check for every known to myself signal that are errros

--- src/command/command_status.c
+++ src/command/command_status.c
@@ -7,8 +7,30 @@
 
 #include "my.h"
 
+static int do_core_dump(int status)
+{
+    if (WCOREDUMP(status))
+        my_dputs(2, "(core dump)");
+    my_dputs(2, "\n");
+    return 0;
+}
+
 int command_status(void *shell_ptr, int status)
 {
-    shell_set_code(shell_ptr, status);
+    if (WIFEXITED(status))
+        return shell_set_code(shell_ptr, WEXITSTATUS(status));
+    if (WTERMSIG(status) == SIGSEGV) {
+        my_dputs(2, "Segmentation fault ");
+        do_core_dump(status);
+    }
+    if (WTERMSIG(status) == SIGILL) {
+        my_dputs(2, "Illegal instruction ");
+        do_core_dump(status);
+    }
+    if (WTERMSIG(status) == SIGTRAP) {
+        my_dputs(2, "Trace/BPT trap ");
+        do_core_dump(status);
+    }
+    shell_set_code(shell_ptr, WTERMSIG(status) + 128);
     return 0;
 }
