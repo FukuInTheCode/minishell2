1bf4df41f8f46261895149ea57ffcdb9ba3a3415
Tom C <tom.carlier@epitech.eu>
2024-03-27 11:38:36 +0100

    [feat] -->  command now don't fork if it is a builtin in last position

--- src/command/command_exec.c
+++ src/command/command_exec.c
@@ -9,12 +9,18 @@
 
 static int do_child_process(command_t *command, shell_t *shell)
 {
+    int tmp = 0;
+
     if (command->in != SYS_IN)
         dup2(command->in, 0);
     if (command->out != SYS_OUT)
         dup2(command->out, 1);
-    if (command_is_builtin(command))
-        exit(command_builtins(command, (void *)shell));
+    if (command_is_builtin(command)) {
+        tmp = command_builtins(command, (void *)shell);
+        if (command->out != 1)
+            exit(tmp);
+        return tmp;
+    }
     if (execve(command->argv[0], command->argv, shell->env) == -1)
         return command_error(command, errno, shell);
     return 0;
@@ -36,9 +42,12 @@ static int do_parent_process(command_t *command, pid_t pid, shell_t *shell)
 int command_exec(command_t *command, void *shell_ptr)
 {
     pid_t pid = 0;
+    bool is_builtin = false;
 
     command_path(command, shell_ptr);
-    pid = fork();
+    is_builtin = command_is_builtin(command);
+    if (!is_builtin || command->out != 1)
+        pid = fork();
     if (pid < 0) {
         perror("fork");
         return 84;
