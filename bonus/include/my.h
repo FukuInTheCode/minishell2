288ec9e3175d255acaf160112a43c18cc3716d4a
Tom C <tom.carlier@epitech.eu>
2024-03-30 21:37:55 +0100

    [feat] -->  add the bonus folder and it base build context files

---
+++ bonus/include/my.h
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT, 2023
+** MY_H
+** File description:
+** Placeholder
+*/
+
+#pragma once
+    #define MY_H
+
---
+++ bonus/src/main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** main.c
+** File description:
+** main.c
+*/
+
+#include "my.h"
+
+int main(int argc, char **argv, char **envp)
+{
+    return 84;
+}
